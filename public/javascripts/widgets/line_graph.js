aff18a549245b994f2738e27bae97aff46162a68
Victor Martinez <victor.martinez@migcan.com>
2010-05-01 17:19:44 +0100

    WIP: drawing graph in line chart widget.

--- public/javascripts/widgets/line_graph.js
+++ public/javascripts/widgets/line_graph.js
@@ -4,32 +4,47 @@ var LineGraph = Class.create(Widget, {
     return($super(widget_id, config));
   },
   receive: function(payload) {
-      //this.messages = [];
-    //payload.each(function(message) {
       this.messages.push( payload );
-      //}.bind(this));
-    this.update();
-  },
+      this.update();
+    },
   update: function() {
     new Draggable($(this.container));
+
+    var position = Element.positionedOffset($(this.container))
+
     this.container.childElements().invoke('remove');
     this.container.appendChild(this.buildWidgetIcon());
     this.container.appendChild(this.buildHeader());
+    this.container.appendChild(this.buildGraphArea());
+
+    var values = [];
+    var xs = [];
+
+    for (i=1; i<=20; i++){
+      values.push(0);
+      xs.push(i);
+    }
+    var i=0;
     this.messages.each(function(message) {
-      var cont = new Element('p', { id: message.id});
-
-      data = new Element('span', { className: 'status' });
-      data.appendChild(document.createTextNode(message.time + " : " +message.value));
-      cont.appendChild(data);
-
-      this.container.appendChild(cont);
-      // new Effect.Pulsate(this.container, { pulses: 2, duration: 1 });
+        values[++i] = message.value;
     }.bind(this));
+
+    var paper = Raphael("graph");
+    //paper.g.piechart(120, 150, 100, values);
+
+    var opts = {nostroke: true, axis: "0 0 1 1", symbol: "x"}
+            //axminx: .8, axmaxx: 1.2, axminy: .8, axmaxy: 1.2};
+
+    paper.g.linechart(30,80,200,250, xs, values, opts);
+
   },
   buildWidgetIcon: function() {
-    return(new Element("img", {src: "images/tfl.png", width: 32, height: 32, className: 'tfl icon'}));
-  },
-  buildHeader: function() {
-    return(new Element("h2").update(this.title));
-  }
+      return(new Element("img", {src: "images/graph.png", width: 32, height: 32, className: 'graph icon'}));
+    },
+      buildHeader: function() {
+      return(new Element("h2").update(this.title));
+    },
+      buildGraphArea: function() {
+      return(new Element("div", {id: "graph"}));
+    }
 });
